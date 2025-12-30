7f91d74a30cfe1a60f2fe97130301a8a2f099af3
harsh-eagle-sigmoid <harshal.anil@sigmoidanalytics.com>
2025-12-30 19:18:06 +0530

    embedding_model.py

--- embeddings/embedding_model.py
+++ embeddings/embedding_model.py
@@ -2,5 +2,7 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def get_embedding_model():
     return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
     )
